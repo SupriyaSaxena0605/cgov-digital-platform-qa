a38fcc48b9703d5d80240917fe5478bd50307d18
Saxena <saxenas3@nih.gov>
2019-04-30 10:09:03 -0400

    Added tests to Leadimage

--- src/main/java/gov/cancer/pageobject/crosscutting/LeadImage.java
+++ src/main/java/gov/cancer/pageobject/crosscutting/LeadImage.java
@@ -3,31 +3,57 @@ package gov.cancer.pageobject.crosscutting;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 
 import gov.cancer.framework.ElementHelper;
 import gov.cancer.pageobject.PageObjectBase;
 
 public class LeadImage extends PageObjectBase {
 
-  
+  @FindBy(how = How.CSS, using = "#cgvBody")
+  WebElement pageBody;
+
   /********* FOOTER SELECTORS ***********************/
 
-  final public String imagecontainer = ".embedded-entity";
-  
+  final public String imagecontainer = "div#cgvBody > div > figure";
+  final public String imagecaption = "div.caption-container p:nth-of-type(2)";
+  final public String imagecredit = ".image-photo-credit";
+  final public String imagediv = "div[class='centered-element'] img";
+
   /**
    * Constructor
    *
-   * @param path server-relative path of the page to load.
+   * @param path
+   *          server-relative path of the page to load.
    */
   public LeadImage(String path) {
     super(path);
-     
 
   }
-  
+
   /* Returns true if Lead image is displayed on the page */
   public boolean isLeadImageVisible() {
-    return ElementHelper.findElement(getBrowser(), imagecontainer).isDisplayed();
+    return ElementHelper.isVisible(getBrowser(), imagecontainer);
   }
+
+  /* Returns the caption of the image */
+  public String getCaption() {
+    return ElementHelper.findElement(getBrowser(), imagecaption).getText();
+  }
+
+  /* Returns the credit of the image */
+  public String getCredit() {
+
+    return ElementHelper.findElement(getBrowser(), imagecredit).getText();
+  }
+
+  /* Returns the alt text of the image */
+  public String getAltText() {
+    WebElement ImageDiv = ElementHelper.findElement(getBrowser(), imagediv);
+    return ImageDiv.getAttribute("alt");
+  }
+  
 }
--- src/test/java/gov/cancer/tests/crosscutting/LeadImage_Test.java
+++ src/test/java/gov/cancer/tests/crosscutting/LeadImage_Test.java
@@ -8,53 +8,120 @@ import org.testng.annotations.Test;
 
 import gov.cancer.framework.ExcelDataReader;
 import gov.cancer.pageobject.crosscutting.FooterPage;
+import gov.cancer.pageobject.crosscutting.LeadImage;
 import gov.cancer.tests.TestObjectBase;
 import gov.cancer.tests.TestRunner;
 
 /**
  * Tests for pages with lead images.
  */
-public class LeadImage_Test extends TestObjectBase{
-  
-  
-    final public String Footer_header = "National Cancer Institute";
-    final public String Footer_headerEsp = "Instituto Nacional del Cáncer";
+public class LeadImage_Test extends TestObjectBase {
 
-    /**
-     * This method is checking if the Footer exists on the pages
-     * 
-     * @param path
-     *          Path of the page to check.
-     * @param language
-     *          Language of the page to check.
-     */
-    @Test(dataProvider = "getPageFooterPaths")
-    public void footerIsVisible(String path, String caption, String credit, String alt) {
+  /**
+   * This method is checking if the Footer exists on the pages
+   * 
+   * @param path
+   *          Path of the page to check.
+   * @param language
+   *          Language of the page to check.
+   */
+   //@Test(dataProvider = "getPageWithLeadImage")
+  public void isLeadImageVisible(String path, String caption, String credit, String alt) {
 
-      TestRunner.run(FooterPage.class, path, (FooterPage page) -> {
+    TestRunner.run(LeadImage.class, path, (LeadImage page) -> {
 
-        Assert.assertTrue(page.isFooterVisible(), "Page Footer is visible.");
+      Assert.assertTrue(page.isLeadImageVisible(), "Lead Image is visible.");
 
-      });
+    });
 
-    }
-    
-    /*******************************************
-    * DATA PROVIDER
-    *******************************************/
+  }
+   
+   @Test(dataProvider = "getPageWithoutLeadImage")
+   public void verifyLeadImagenotVisible(String path) {
 
-   /**
-    * Retrieves a list of paths to pages which are expected to have Footer
-    * 
-    * @return An iterable list of two element arrays, each containing a path and
-    *         language.
-    */
-   @DataProvider(name = "getPageWithLeadImage")
-   public Iterator<Object[]> getPageWithLeadImage() {
-     String[] columns = { "path", "Caption", "Credit","Alt text" };
-     return new ExcelDataReader(getDataFilePath("leadimage-data.xlsx"), "pages_with_leadimage", columns);
+     TestRunner.run(LeadImage.class, path, (LeadImage page) -> {
+
+       Assert.assertFalse(page.isLeadImageVisible(), "Lead Image is not visible.");
+
+     });
 
    }
 
-    
+
+  // @Test(dataProvider = "getPageWithLeadImage_Caption")
+  public void verifyImageCaption(String path, String caption) {
+
+    TestRunner.run(LeadImage.class, path, (LeadImage page) -> {
+
+      Assert.assertTrue(page.getCaption().equals(caption), "Lead Image caption is correct");
+
+    });
+
+  }
+
+ //@Test(dataProvider = "getPageWithLeadImage_Credit")
+  public void verifyImageCredit(String path, String credit) {
+
+    TestRunner.run(LeadImage.class, path, (LeadImage page) -> {
+
+      Assert.assertTrue(page.getCredit().matches(credit), "Lead Image credit is correct");
+
+    });
+
+  }
+
+  //@Test(dataProvider = "getPageWithLeadImage_Alt")
+  public void verifyImageAltText(String path, String alt) {
+
+    TestRunner.run(LeadImage.class, path, (LeadImage page) -> {
+      Assert.assertTrue(page.getAltText().equals(alt), "Lead Image alt is correct");
+      ;
+
+    });
+
+  }
+
+  /*******************************************
+   * DATA PROVIDER
+   *******************************************/
+
+  /**
+   * Retrieves a list of paths to pages which are expected to have Lead Image
+   * 
+   * @return An iterable list of two element arrays, each containing a path and
+   *         language.
+   */
+  @DataProvider(name = "getPageWithLeadImage")
+  public Iterator<Object[]> getPageWithLeadImage() {
+    String[] columns = { "path", "Caption", "Credit", "Alt text" };
+    return new ExcelDataReader(getDataFilePath("leadimage-data.xlsx"), "pages_with_leadimage", columns);
+
+  }
+
+  @DataProvider(name = "getPageWithLeadImage_Caption")
+  public Iterator<Object[]> getPageWithLeadImage_Caption() {
+    String[] columns = { "path", "Caption" };
+    return new ExcelDataReader(getDataFilePath("leadimage-data.xlsx"), "pages_with_leadimage_Caption", columns);
+
+  }
+
+  @DataProvider(name = "getPageWithLeadImage_Alt")
+  public Iterator<Object[]> getPageWithLeadImage_Alt() {
+    String[] columns = { "path", "Alt text" };
+    return new ExcelDataReader(getDataFilePath("leadimage-data.xlsx"), "pages_with_leadimage_Alt", columns);
+
+  }
+
+  @DataProvider(name = "getPageWithLeadImage_Credit")
+  public Iterator<Object[]> getPageWithLeadImage_Credit() {
+    String[] columns = { "path", "Credit" };
+    return new ExcelDataReader(getDataFilePath("leadimage-data.xlsx"), "pages_with_leadimage_Credit", columns);
+
+  }
+  @DataProvider(name = "getPageWithoutLeadImage")
+  public Iterator<Object[]> getPageWithoutLeadImage() {
+    String[] columns = { "path" };
+    return new ExcelDataReader(getDataFilePath("leadimage-data.xlsx"), "pages_with_noleadimage", columns);
+
+  }
 }
